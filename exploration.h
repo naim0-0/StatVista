ed059e5f1d758b0121a2b43901fd3c310230f2d2
Naim Islam <naim107517@gmail.com>
2026-01-11 22:27:02 +0600

    Add files via upload

--- exploration.h
+++ exploration.h
@@ -6,4 +6,16 @@ double calculateMedian();
 double calculateMin();
 double calculateMax();
 
+double calculateVariance();
+double calculateStandardDeviation();
+double calculateRange();
+
+void calculateFrequency();
+double calculateCorrelation();
+
+double calculateQ1();
+double calculateQ3();
+double calculateIQR();
+void detectOutliers();
+
 #endif
